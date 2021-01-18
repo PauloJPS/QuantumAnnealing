22bf449a42fed3641b596d3efee1d956e108ead8
Paulo <paulo.paulino.souza96@gmail.com>
2021-01-18 08:59:58 -0300

    add codes

---
+++ Grover.py
@@ -0,0 +1,75 @@
+import numpy as np 
+import qutip as qt 
+
+def Grover_Hi_Hf(m, N):
+    """
+        Receive the number of qubits and return the initial Hamiltonian for the Grover problem
+        ---------
+        Parameters:
+            N : (Number of qubits - int)
+            m : (index of the market state - int)
+        Return:
+            Hi : A qutip object representing the initial Hamiltonian for the Grover problem 
+            Hf : A qutip object representing the final Hamiltonian for the Grover problem 
+
+    """
+    I = GetIdentity(N)
+    eigenstates = I.eigenstates()[1]
+    psi = np.sqrt(1/2**N) * np.sum(eigenstates)
+    ket_m = eigenstates[m]
+
+    Hi = I - psi * psi.dag()
+    Hf = I - ket_m * ket_m.dag() 
+    return Hi, Hf
+
+def GetIdentity(N):
+    """
+        Receive the number of qubits and return the Identity matrix for its Hilbert space 
+        This is necessary because we want to keep the tensorial composition form 
+        Parameters:
+            N : (Number of qubits - int)
+        Return:
+            A qutip object representing a Identity matrix (2^N X 2^N)
+    """
+    I = np.zeros(N, dtype=object)
+    for i in range(N):
+        I[i] = qt.identity(2) 
+    I = qt.tensor(I)
+    return I 
+
+def Spectrum_alongInterpolation(Hi, Hf, psii, As, Bs, tlist):
+    """
+        Return the spectrum of the time-dependent Hamiltonian for each time interval
+        Parameters: 
+            Hi : Initial Hamiltonian (qutip object)
+            Hf : Final Hamiltonian (qutip object)
+            As : Parametrization function (python function)
+            Bs : Parametrization function (python function)
+            tlist : time list (np.array)
+        Return: 
+            list of eigenenergies for each time interval in tlist 
+
+    """
+    args = {'t_max': tlist[-1]} 
+    return np.array([(As(i, args)*Hi + Bs(i, args)*Hf).eigenenergies() for i in tlist])
+    
+
+def QuantumAnnealing(Hi, Hf, psii, As, Bs, tlist):
+    """
+        Return the system states along the interpolation for a given parametrization and a time list    
+        Parameters: 
+            Hi : Initial Hamiltonian (qutip object)
+            Hf : Final Hamiltonian (qutip object)
+            psii : Initial State (qutip object)
+            As : Parametrization function (python function)
+            Bs : Parametrization function (python function)
+            tlist : time list (np.array)
+        Return: 
+            system state in each interpolation time interval 
+    """
+    Ht = [[Hi, As], [Hf, Bs]]
+      
+    args = {'t_max': tlist[-1]} 
+    result = qt.sesolve(H=Ht, psi0=psii, tlist=tlist, args=args)
+    return result.s
+
---
+++ plotTopologies.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import dwave_networkx as dnx
+import networkx as nx
+
+G_chimera = dnx.chimera_graph(3, 3, 4)
+G_pegasus = dnx.pegasus_graph(3)
+pos_chimera = dnx.chimera_layout(G_chimera)
+pos_pegasus = dnx.pegasus_layout(G_pegasus)
+
+
+
+fig, ax = plt.subplots(figsize=(6,6))
+nx.draw_networkx_nodes(G_chimera, pos_chimera, node_color='#377eb8', edgecolors='black', node_size=100, ax=ax)
+nx.draw_networkx_edges(G_chimera, pos_chimera, ax=ax)
+plt.axis('off')
+ax.get_xaxis().set_visible(False) # this removes the ticks and numbers for x axis
+ax.get_yaxis().set_visible(False) # this removes the ticks and numbers for y axis
+plt.savefig('chimera.pdf', bbox_inches='tight',pad_inches = 0, dpi = 200)
+plt.close()
+
+fig, ax = plt.subplots(figsize=(6,6))
+nx.draw_networkx_nodes(G_pegasus, pos_pegasus, node_color='#e41a1c', edgecolors='black', node_size=100, ax=ax)
+nx.draw_networkx_edges(G_pegasus, pos_pegasus, ax=ax)
+plt.axis('off')
+ax.get_xaxis().set_visible(False) # this removes the ticks and numbers for x axis
+ax.get_yaxis().set_visible(False) # this removes the ticks and numbers for y axis
+plt.savefig('pegasus.pdf', bbox_inches='tight',pad_inches = 0, dpi = 200)
+
+
+
